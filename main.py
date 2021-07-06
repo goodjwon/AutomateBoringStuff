c04761a9010f3284e9e94a438e457171011ef89e
JungWon Park <firstman9@protonmail.com>
2021-07-06 23:11:00 +0900

    chapter1  - Your First Program

--- main.py
+++ main.py
@@ -6,3 +6,26 @@ print(2+3*6)
 
 print((2+3)*6)
 
+print('Hello world')
+
+print('Hello world' + 'Sophia')
+
+print('sophia ' * 5)
+
+spam = 40
+
+print(spam)
+
+eggs = 2
+
+print(eggs)
+
+print(spam + eggs)
+
+spam = 'Hello'
+
+print(spam)
+
+spam = 'GoodBye'
+
+print(spam)
